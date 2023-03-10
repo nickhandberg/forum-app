c4ebfc5d72c9d585c2b1f68928be35f55294706a
noko12312 <42224565+noko12312@users.noreply.github.com>
2023-03-09 20:34:57 -0600

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import "./index.css";
+
+function App() {
+    return (
+        <div className="App">
+            <p className="mt-6 text-black bg-white">Test</p>
+            <p className="mt-6 text-white bg-black">Test</p>
+        </div>
+    );
+}
+
+export default App;
