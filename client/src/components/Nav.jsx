e2055809442569005e7b534f281821090ced7264
noko12312 <42224565+noko12312@users.noreply.github.com>
2023-03-09 23:11:54 -0600

    nav bar border change

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const Nav = () => {
     return (
         <header>
-            <nav className=" w-full h-auto  bg-light-1 dark:bg-dark-2 dark:text-dark-text-1 flex p-2 px-14 rounded-bl-[50px] border-light-text-2 border-b-[5px] justify-between">
-                <h1 className="text-2xl my-auto font-bold">Forum</h1>
-                <div className="flex-shrink m-auto text-lg font-medium hidden md:flex lg:flex space-x-28">
+            <nav className=" w-full h-auto  bg-light-1 dark:bg-dark-2 dark:text-dark-text-1 flex p-2 px-14 rounded-bl-[50px] border-b-[2px] border-dark-3 justify-between items-center">
+                <h1 className="text-2xl  font-bold">Forum</h1>
+                <div className="flex-shrink  text-md font-medium hidden md:flex lg:flex space-x-28">
                     <a>frontpage</a>
                     <a>popular</a>
                     <a>new</a>
@@ -15,7 +15,7 @@ const Nav = () => {
                         color="#6fc938"
                         type="solid"
                         name="user-circle"
-                        size="lg"
+                        size="md"
                     ></box-icon>
                 </div>
             </nav>
