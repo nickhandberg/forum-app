1dba14ddace1a7859fbbd7cad09609614614d598
noko12312 <42224565+noko12312@users.noreply.github.com>
2023-03-16 19:40:19 -0500

    t

--- server/config/corsOptions.js
+++ server/config/corsOptions.js
@@ -1,20 +1,13 @@
 const whitelist = require("./whitelist");
 
 const corsOptions = {
-    // origin: (origin, callback) => {
-    //     // NOTE: REMOVE !origin on production
-    //     if (whitelist.indexOf(origin) !== -1) {
-    //         callback(null, true);
-    //     } else {
-    //
-    //         callback(new Error("Not allowed by CORS"));
-    //     }
-    // },
     origin: "https://sawwit.netlify.app",
 
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["*"],
 
-    allowedHeaders: ["*"],
+    allowedHeaders: [
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+    ],
     optionsSuccessStatus: 200,
     credentials: true,
 };
