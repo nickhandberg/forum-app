d58ee9426680f6bd51e10029e753e894f26a3012
noko12312 <42224565+noko12312@users.noreply.github.com>
2023-03-18 08:07:03 -0500

    mobile considerations. needs cleaning up

--- client/src/components/PostContent.jsx
+++ client/src/components/PostContent.jsx
@@ -4,16 +4,16 @@ import Icon from "../components/Icon";
 import useAppContext from "../hooks/useAppContext";
 import useAxiosPrivate from "../hooks/useAxiosPrivate";
 import {
-    comment,
-    downvote,
-    downvoteFilled,
+    altDownvote,
+    altUpvote,
+    channelIcon,
+    dotMenu,
     editIcon,
     linkExternal,
+    signIn,
     star,
     starFilled,
     trashcan,
-    upvote,
-    upvoteFilled,
 } from "../img/iconPaths";
 import { getDomain } from "../utils/getDomain";
 import { getPostAge } from "../utils/getPostAge";
@@ -34,6 +34,7 @@ const PostContent = ({
     const [saved, setSaved] = useState(false);
     const [confirm, setConfirm] = useState(false);
     const [showEditForm, setShowEditForm] = useState(false);
+    const [showContextMenu, setShowContextMenu] = useState(false);
     const [editedText, setEditedText] = useState(self_text);
     const [linkData, setLinkData] = useState({});
     const editRef = useRef();
@@ -99,7 +100,7 @@ const PostContent = ({
     };
 
     return (
-        <div className="bg-light-1 dark:text-light-1 dark:bg-dark-2 md:p-4 md:rounded-md w-full md:max-w-[1000px] flex-col flex justify-between">
+        <div className="bg-light-1 dark:text-light-1 dark:bg-dark-2  md:rounded-md w-full md:max-w-[1000px] flex-col flex justify-between">
             {confirm && (
                 <div className="flex flex-col w-[100vw] h-[100vh] absolute top-0 left-0 z-10 backdrop-blur-sm ">
                     <div className="absolute top-0 bottom-0 left-0 right-0 m-auto text-center max-w-max max-h-min md:rounded-lg bg-light-1 dark:bg-dark-2">
@@ -124,38 +125,8 @@ const PostContent = ({
                 </div>
             )}
 
-            <div className="flex flex-col p-4 md:p-0">
-                <h1 className="text-xl md:text-2xl  font-semibold mb-2">
-                    {title}
-                </h1>
-                <div className="flex flex-col md:flex-row md:gap-8 mb-2">
-                    <p
-                        className="text-green-1 hover:underline w-min dark:text-green-1 cursor-pointer"
-                        onClick={() => navigate(`/c/${channel}`)}
-                    >
-                        {channel}
-                    </p>
-                    <p className="dark:text-light-2 text-sm md:text-base">
-                        Posted by{" "}
-                        <span
-                            onClick={() => navigate(`/u/${username}`)}
-                            className="text-green-1 hover:underline cursor-pointer"
-                        >
-                            {username}
-                        </span>{" "}
-                        {getPostAge(age)}
-                    </p>
-                </div>
-            </div>
-
             {image && !link && (
-                <img className=" max-h-[720px] w-min m-auto" src={image}></img>
-            )}
-
-            {self_text && (
-                <pre className=" bg-light-3 dark:bg-dark-3 overflow-hidden p-4 text-lg md:rounded-md h-full dark:text-light-2">
-                    {self_text}
-                </pre>
+                <img className=" max-h-[720px] w-auto m-auto" src={image}></img>
             )}
 
             {link && !image && (
@@ -193,96 +164,259 @@ const PostContent = ({
                     </div>
                 </div>
             )}
-            <div className="flex justify-between mt-4 py-4 md:py-0 px-6 max-w-[400px]">
-                <div className="flex gap-2">
+
+            <div className="flex flex-col px-4 pt-4 z-10">
+                <h1 className="text-lg md:text-2xl dark:text-light-1 font-semibold">
+                    {title}
+                </h1>
+                <div className="flex flex-col md:flex-row md:gap-8 md:mt-1">
+                    <p
+                        className="text-green-1 text-sm md:text-base hover:underline w-min cursor-pointer"
+                        onClick={(e) => redirect(e, `/c/${channel}`)}
+                    >
+                        {channel}
+                    </p>
+                    <p className="dark:text-light-2 text-xs md:text-base">
+                        Posted by{" "}
+                        <span
+                            onClick={(e) => redirect(e, `/u/${username}`)}
+                            className="text-green-1 hover:underline cursor-pointer"
+                        >
+                            {username}
+                        </span>{" "}
+                        {getPostAge(age)}
+                    </p>
+                </div>
+            </div>
+
+            {self_text && (
+                <pre className=" overflow-hidden p-4 text-lg md:rounded-md h-full dark:text-light-2">
+                    {self_text}
+                </pre>
+            )}
+
+            <div className="flex justify-between mt-4 pb-4  px-4">
+                <div className="flex flex-col text-xs md:text-sm dark:text-light-2">
+                    <p>
+                        {karma > 1000 ? (karma / 1000).toFixed(1) + "k" : karma}{" "}
+                        points
+                    </p>
+                    <p>152 comments</p>
+                </div>
+
+                <div className="flex items-center gap-6 md:gap-14">
                     <button
                         onClick={(e) => {
                             handleUpvote(e);
                         }}
                     >
                         <Icon
-                            path={upvoted ? upvoteFilled : upvote}
+                            //path={upvoted ? upvoteFilled : upvote}
+                            path={altUpvote}
                             fill={
                                 upvoted
                                     ? "#6fc938"
                                     : darkMode
                                     ? "#c4c4c4"
-                                    : "#161617"
+                                    : "#68696b"
                             }
-                            stroke={darkMode ? "#c4c4c4" : "#161617"}
-                            w={"30px"}
-                            h={"30px"}
+                            stroke={
+                                upvoted
+                                    ? "#6fc938"
+                                    : darkMode
+                                    ? "#c4c4c4"
+                                    : "#68696b"
+                            }
+                            w={"35px"}
+                            h={"35px"}
                         />
                     </button>
-                    <p className="dark:text-light-2 text-lg text-center">
-                        {karma > 1000 ? (karma / 1000).toFixed(1) + "k" : karma}
-                    </p>
+
                     <button
                         onClick={(e) => {
                             handleDownvote(e);
                         }}
                     >
                         <Icon
-                            path={downvoted ? downvoteFilled : downvote}
+                            //path={downvoted ? downvoteFilled : downvote}
+                            path={altDownvote}
                             fill={
                                 downvoted
                                     ? "#d90f63"
                                     : darkMode
                                     ? "#c4c4c4"
-                                    : "#161617"
+                                    : "#68696b"
                             }
-                            stroke={darkMode ? "#c4c4c4" : "#161617"}
+                            stroke={
+                                downvoted
+                                    ? "#d90f63"
+                                    : darkMode
+                                    ? "#c4c4c4"
+                                    : "#68696b"
+                            }
+                            w={"35px"}
+                            h={"35px"}
+                        />
+                    </button>
+                    <button
+                        className="flex align-middle"
+                        onClick={(e) => handleSave(e)}
+                    >
+                        <Icon
+                            path={saved ? starFilled : star}
+                            fill={
+                                saved
+                                    ? "#d6c106"
+                                    : darkMode
+                                    ? "#c4c4c4"
+                                    : "#68696b"
+                            }
+                            stroke={
+                                saved
+                                    ? "#d6c106"
+                                    : darkMode
+                                    ? "#c4c4c4"
+                                    : "#68696b"
+                            }
                             w={"30px"}
                             h={"30px"}
                         />
                     </button>
-                </div>
+                    {auth?.username === username && (
+                        <button
+                            className=" items-center gap-2 hidden md:flex"
+                            onClick={() => setConfirm(true)}
+                        >
+                            <Icon
+                                path={trashcan}
+                                fill={darkMode ? "#c4c4c4" : "#161617"}
+                                stroke={darkMode ? "#c4c4c4" : "#161617"}
+                                w={"30px"}
+                                h={"30px"}
+                            />
+                        </button>
+                    )}
+                    {auth?.username === username && self_text != "" && (
+                        <button
+                            className=" items-center gap-2 hidden md:flex"
+                            onClick={() => {
+                                setShowEditForm(!showEditForm);
+                            }}
+                        >
+                            <Icon
+                                path={editIcon}
+                                fill={darkMode ? "#c4c4c4" : "#161617"}
+                                stroke={darkMode ? "#c4c4c4" : "#161617"}
+                                w={"30px"}
+                                h={"30px"}
+                            />
+                        </button>
+                    )}
 
-                <button
-                    className="flex align-middle"
-                    onClick={(e) => handleSave(e)}
-                >
-                    <Icon
-                        path={saved ? starFilled : star}
-                        fill={
-                            saved ? "#d6c106" : darkMode ? "#c4c4c4" : "#161617"
-                        }
-                        stroke={darkMode ? "#c4c4c4" : "#161617"}
-                        w={"30px"}
-                        h={"30px"}
-                    />
-                </button>
-                {auth?.username === username && (
-                    <button
-                        className="flex align-middle"
-                        onClick={() => setConfirm(true)}
-                    >
-                        <Icon
-                            path={trashcan}
-                            fill={darkMode ? "#c4c4c4" : "#161617"}
-                            stroke={darkMode ? "#c4c4c4" : "#161617"}
-                            w={"30px"}
-                            h={"30px"}
-                        />
-                    </button>
-                )}
-                {auth?.username === username && self_text != "" && (
-                    <button
-                        className="flex align-middle"
-                        onClick={() => {
-                            setShowEditForm(!showEditForm);
-                        }}
-                    >
-                        <Icon
-                            path={editIcon}
-                            fill={darkMode ? "#c4c4c4" : "#161617"}
-                            stroke={darkMode ? "#c4c4c4" : "#161617"}
-                            w={"30px"}
-                            h={"30px"}
-                        />
-                    </button>
-                )}
+                    <div className="relative">
+                        <button
+                            onClick={() => {
+                                setShowContextMenu(!showContextMenu);
+                            }}
+                            className="block md:hidden"
+                        >
+                            <Icon
+                                path={dotMenu}
+                                fill={darkMode ? "#c4c4c4" : "#68696b"}
+                                stroke={darkMode ? "#c4c4c4" : "#68696b"}
+                                w={"30px"}
+                                h={"30px"}
+                            />
+                        </button>
+                        {showContextMenu && (
+                            <div className="absolute right-[-16px] text-xl dark:text-light-2 bg-light-3 dark:bg-dark-3 border-b-2 border-l-2 border-dark-1 md:hidden flex flex-col p-5 mt-[18px] gap-4">
+                                <button
+                                    className="flex items-center gap-2"
+                                    onClick={() => {
+                                        navigate(`/c/${channel}`);
+                                    }}
+                                >
+                                    <Icon
+                                        path={channelIcon}
+                                        fill={darkMode ? "#c4c4c4" : "#161617"}
+                                        stroke={
+                                            darkMode ? "#c4c4c4" : "#161617"
+                                        }
+                                        w={"30px"}
+                                        h={"30px"}
+                                    />
+                                    {channel}
+                                </button>
+
+                                <button
+                                    className="flex items-center gap-2"
+                                    onClick={() => {
+                                        navigate(`/u/${username}`);
+                                    }}
+                                >
+                                    <Icon
+                                        path={signIn}
+                                        fill={darkMode ? "#c4c4c4" : "#161617"}
+                                        stroke={
+                                            darkMode ? "#c4c4c4" : "#161617"
+                                        }
+                                        w={"30px"}
+                                        h={"30px"}
+                                    />
+                                    {username}
+                                </button>
+
+                                {auth?.username === username &&
+                                    self_text != "" && (
+                                        <button
+                                            className="flex items-center gap-2"
+                                            onClick={() => {
+                                                setShowEditForm(!showEditForm);
+                                            }}
+                                        >
+                                            <Icon
+                                                path={editIcon}
+                                                fill={
+                                                    darkMode
+                                                        ? "#c4c4c4"
+                                                        : "#161617"
+                                                }
+                                                stroke={
+                                                    darkMode
+                                                        ? "#c4c4c4"
+                                                        : "#161617"
+                                                }
+                                                w={"30px"}
+                                                h={"30px"}
+                                            />
+                                            Edit
+                                        </button>
+                                    )}
+                                {auth?.username === username && (
+                                    <button
+                                        className="flex items-center gap-2"
+                                        onClick={() => setConfirm(true)}
+                                    >
+                                        <Icon
+                                            path={trashcan}
+                                            fill={
+                                                darkMode ? "#c4c4c4" : "#161617"
+                                            }
+                                            stroke={
+                                                darkMode ? "#c4c4c4" : "#161617"
+                                            }
+                                            w={"30px"}
+                                            h={"30px"}
+                                        />
+                                        Delete
+                                    </button>
+                                )}
+                            </div>
+                        )}
+                    </div>
+                </div>
             </div>
+
             {showEditForm && (
                 <form onSubmit={handleEdit} className="flex flex-col gap-2 p-4">
                     <div className="flex flex-col gap-2">
